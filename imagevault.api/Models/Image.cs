61624feb26793a5641a611141115dbf610fe8eb2
houstonsoper <houstonsoper@gmail.com>
2025-02-16 06:39:52 +0000

    Added Image and Post models

---
+++ imagevault.api/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imagevault.api.Models;
+
+public class Image
+{
+	public Guid Id { get; set; } = Guid.NewGuid();
+	public required IFormFile ImageFile { get; set; }
+}
---
+++ imagevault.api/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imagevault.api.Models;
+
+public class Post
+{
+	public Guid Id { get; set; } = Guid.NewGuid();
+	
+	[Required (ErrorMessage=("Title is required"))]
+	[StringLength(100, ErrorMessage ="Title must be between 5 and 100 characters", MinimumLength = 5)]
+	public required string Title { get; set; } 
+	
+	[Required (ErrorMessage=("Description is required"))]
+	[StringLength(200, ErrorMessage ="Description must be between 5 and 200 characters", MinimumLength = 5)]
+	public required string Description { get; set; } 
+	
+	public DateTime Date { get; set; } = DateTime.UtcNow;
+	
+	public bool IsActive { get; set; } = true;
+	
+	[Required (ErrorMessage ="Please enter a User Id")]
+	public required string UserId { get; set; }
+	
+	//Navigation
+	public User? User { get; set; }
+}
