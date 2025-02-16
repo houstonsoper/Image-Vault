e4768d513f2a174f776ade367614a86b8b4c3e5f
houstonsoper <houstonsoper@gmail.com>
2025-02-16 06:47:03 +0000

    Added a CreatePostDto as well as an extension to convert it to a Post

---
+++ imagevault.api/Dtos/CreatePostDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imagevault.api.DTOs;
+
+public class CreatePostDto
+{
+	[Required (ErrorMessage=("Title is required"))]
+	[StringLength(100, ErrorMessage ="Title must be between 5 and 100 characters", MinimumLength = 5)]
+	public required string Title { get; set; } 
+	
+	[Required (ErrorMessage=("Description is required"))]
+	[StringLength(200, ErrorMessage ="Description must be between 5 and 200 characters", MinimumLength = 5)]
+	public required string Description { get; set; } 
+	
+	[Required (ErrorMessage ="Please enter a User Id")]
+	public required string UserId { get; set; }
+}
---
+++ imagevault.api/Extensions/PostExtension.cs
@@ -0,0 +1,20 @@
+﻿using imagevault.api.DTOs;
+using imagevault.api.Models;
+
+namespace imagevault.api.Extensions;
+
+public static class PostExtension
+{
+	public static Post ToPostFromCreatePostDto (this CreatePostDto dto)
+	{
+		return new Post
+		{
+			Id = default,
+			Title = dto.Title,
+			Description = dto.Description,
+			Date = default,
+			IsActive = false,
+			UserId = dto.UserId
+		};
+	}
+}
