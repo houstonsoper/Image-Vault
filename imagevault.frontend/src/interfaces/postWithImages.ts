2598a36cd0f866f92ee287ae7d27f7702426c960
houstonsoper <houstonsoper@gmail.com>
2025-02-17 16:34:12 +0000

    Added interface for getting Posts and their images

---
+++ imagevault.frontend/src/interfaces/postWithImages.ts
@@ -0,0 +1,11 @@
+﻿import Image from "@/interfaces/image";
+
+export default interface PostWithImages {
+  id: string,
+  title: string,
+  description: string,
+  date: Date,
+  isActive : boolean,
+  userId : string,
+  images : Image[],
+}
