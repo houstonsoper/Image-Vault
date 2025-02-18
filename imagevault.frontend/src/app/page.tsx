11999119a4665660cafd63b2a63026159b092528
houstonsoper <houstonsoper@gmail.com>
2025-02-18 16:50:45 +0000

    Added infinite scrolling to home page

--- imagevault.frontend/src/app/page.tsx
+++ imagevault.frontend/src/app/page.tsx
@@ -1,28 +1,54 @@
 "use client"
 
-import React, { useEffect } from "react"
+import React, {useEffect, useState} from "react"
 import type PostWithImages from "@/interfaces/postWithImages"
 import { fetchPosts } from "@/services/postService"
 import PostCard from "@/components/postCard"
+import {useInView} from "react-intersection-observer";
 
 export default function Home() {
-  const [posts, setPosts] = React.useState<PostWithImages[]>([])
-  const limit = 12
-  const page = 1
+  const [posts, setPosts] = useState<PostWithImages[]>([]);
+  const [nextPosts, setNextPosts] = useState<PostWithImages[]>([]);
+  const [scrollTrigger, isInView] = useInView();
+  const limit = 18;
+  const [page, setPage] = useState<number>(1);
 
+  //Get posts on mount
   useEffect(() => {
-    const controller = new AbortController()
-    const signal: AbortSignal = controller.signal
+    const controller = new AbortController();
+    const signal: AbortSignal = controller.signal;
 
-    const getPosts = async () => {
-      const fetchedPosts: PostWithImages[] = await fetchPosts({ limit, offset: page - 1 }, signal)
-      setPosts(fetchedPosts)
-      console.log(fetchedPosts)
+    const getPosts = async (): Promise<void> => {
+      const fetchedPosts: PostWithImages[] = await fetchPosts({ limit, offset: page - 1 }, signal);
+      setPosts(fetchedPosts);
     }
-    getPosts()
-    return () => controller.abort()
+    getPosts();
+   
+    return () => controller.abort();
   }, [])
+  
+  //Retrieve posts for the next page
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal: AbortSignal = controller.signal;
+    
+    const getNextPosts = async () : Promise<void> => {
+      const fetchedPosts: PostWithImages[] = await fetchPosts({ limit, offset: page * limit}, signal);
+      setNextPosts(fetchedPosts);
+    }
+    getNextPosts();
+    
+    return () => controller.abort()
+  }, [page]);
 
+  //Display the posts for the next page when the scrollTrigger is in view
+  useEffect(() => {
+    if (isInView && nextPosts.length > 0){
+      setPosts(prevPosts => [...prevPosts, ...nextPosts]);
+      setPage(prevPage => prevPage + 1);
+    }
+  }, [isInView]);
+  
   return (
     <section className="py-12">
       <div className="container mx-auto px-12">
@@ -30,6 +56,11 @@ export default function Home() {
           {posts.map((post) => (
             <PostCard post={post} key={post.id}/>
           ))}
+        </div>
+        <div>
+          {nextPosts.length > 0 && (
+            <p className="text-center text-white font-semibold" ref={scrollTrigger}>Loading...</p>
+          )}
         </div>
       </div>
     </section>
