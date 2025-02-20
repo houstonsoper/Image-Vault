3d0bdc0dc2ad3ed626cfde79f63404c71dcd27cb
houstonsoper <houstonsoper@gmail.com>
2025-02-20 14:22:07 +0000

    Added LikeRequest and LikePostDto

---
+++ imagevault.api/Dtos/LikePostDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imagevault.api.DTOs;
+
+public class LikePostDto
+{
+	[Required (ErrorMessage="Post ID is required")]
+	public required Guid PostId { get; set; }
+	
+	[Required (ErrorMessage="User ID is required")]
+	public required Guid UserId { get; set; }
+}
---
+++ imagevault.api/Dtos/LikeRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imagevault.api.DTOs;
+
+public class LikeRequestDto
+{
+	public Guid Id { get; set; }
+	public required Guid PostId { get; set; }
+	public required Guid UserId { get; set; }
+	public DateTime Date { get; set; }
+}
