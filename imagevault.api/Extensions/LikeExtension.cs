72bd67ec1e10b93ca7987785001bb46fe5e6b688
houstonsoper <houstonsoper@gmail.com>
2025-02-20 14:22:35 +0000

    Added ToLike and ToLikeRequestDto extensions

---
+++ imagevault.api/Extensions/LikeExtension.cs
@@ -0,0 +1,29 @@
+﻿using imagevault.api.DTOs;
+using imagevault.api.Models;
+
+namespace imagevault.api.Extensions;
+
+public static class LikeExtension
+{
+	public static Like ToLike(this LikePostDto likePostDto)
+	{
+		return new Like
+		{
+			Id = Guid.NewGuid(),
+			PostId = likePostDto.PostId,
+			UserId = likePostDto.UserId,
+			Date = DateTime.UtcNow
+		};
+	}
+
+	public static LikeRequestDto ToLikeRequestDto(this Like like)
+	{
+		return new LikeRequestDto
+		{
+			Id = like.PostId,
+			PostId = like.PostId,
+			UserId = like.UserId,
+			Date = like.Date,
+		};
+	}
+}
