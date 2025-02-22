c887e74b2517de3ae44e3cbee614d32192d645bd
houstonsoper <houstonsoper@gmail.com>
2025-02-22 17:47:40 +0000

    Added CommentService

---
+++ imagevault.api/Services/CommentService.cs
@@ -0,0 +1,62 @@
+﻿using imagevault.api.Models;
+using imagevault.api.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace imagevault.api.Services;
+
+public class CommentService : ICommentService
+{
+	private readonly IGenericRepository<Comment> _commentRepository;
+	private readonly IUserService _userService;
+	private readonly IPostService _postService;
+
+	public CommentService(IGenericRepository<Comment> commentRepository, IUserService userService, IPostService postService)
+	{
+		_commentRepository = commentRepository;
+		_userService = userService;
+		_postService = postService;
+	}
+	
+	public async Task AddCommentAsync(Comment comment)
+	{
+		//Check Post and User exist
+		await _userService.GetUserByIdAsync(comment.UserId);
+		await _postService.GetPostByIdAsync(comment.PostId);
+		
+		await _commentRepository.AddAsync(comment);
+		await _commentRepository.SaveAsync();
+	}
+
+	public async Task DeleteCommentAsync(Comment comment)
+	{
+		//Check Post and User exist
+		await _userService.GetUserByIdAsync(comment.UserId);
+		await _postService.GetPostByIdAsync(comment.PostId);
+		
+		_commentRepository.Delete(comment);
+		await _commentRepository.SaveAsync();
+	}
+
+	public async Task UpdateCommentAsync(Comment comment)
+	{
+		//Check Post and User exist
+		await _userService.GetUserByIdAsync(comment.UserId);
+		await _postService.GetPostByIdAsync(comment.PostId);
+		
+		_commentRepository.Update(comment);
+		await _commentRepository.SaveAsync();
+	}
+
+	public async Task<List<Comment>> GetCommentsAsync(int? limit, int? offset)
+	{
+		var query = _commentRepository.GetAllQuery();
+
+		if (offset.HasValue && offset != 0)
+			query = query.Skip(offset.Value);
+		
+		if (limit.HasValue && limit.Value != 0)
+			query = query.Take(limit.Value);
+
+		return await query.ToListAsync();
+	}
+}
---
+++ imagevault.api/Services/ICommentService.cs
@@ -0,0 +1,11 @@
+﻿using imagevault.api.Models;
+
+namespace imagevault.api.Services;
+
+public interface ICommentService
+{
+	Task AddCommentAsync(Comment comment);
+	Task DeleteCommentAsync(Comment comment);
+	Task UpdateCommentAsync(Comment comment);
+	Task <List<Comment>> GetCommentsAsync(int? limit, int? offset);
+}
